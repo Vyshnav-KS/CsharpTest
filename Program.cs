2db39176870209919570ee4a9ea1f2aabb52a9c4
Vyshnav-KS <62277580+Vyshnav-KS@users.noreply.github.com>
2021-01-11 15:08:53 +1345

    hello world

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH Local Hack Day Build");
+            Console.WriteLine("You all are great <3");
+            var name = "Vyshnav";
+            Console.WriteLine($"My name is {name}");
+
+        }
+    }
+}
